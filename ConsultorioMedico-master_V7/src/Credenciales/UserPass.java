941ece4fabbae960465db391b6c2c20d48706160
Kevinfcc10 <kevin_01fcc@hotmail.com>
2018-06-06 07:23:35 -0500

    Version 7

---
+++ ConsultorioMedico-master_V7/src/Credenciales/UserPass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Credenciales;
+
+
+public class UserPass {
+    
+    public static String User = "root";
+    public static String Pass = "root";
+    
+}
